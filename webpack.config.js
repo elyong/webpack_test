232b5acbc5190333473142416f56aab97a965244
王健 <1162331666@qq.com>
2020-03-03 19:00:59 +0800

    hello

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,17 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-    entry: {
-        app: './src/index.js',
-        print: './src/print.js'
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist'
-    },
+    entry: './src/index.js',
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            title: 'Output Management'
+            title: 'Caching'
         })
     ],
     output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/'
-    },
+        filename: '[name].[chunkhash].js',
+        path: path.resolve(__dirname, 'dist')
+    }
 };
