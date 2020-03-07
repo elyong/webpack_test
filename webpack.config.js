b748ef9972540b7a3b0ff470f7e96a1de4df854e
王健 <1162331666@qq.com>
2020-03-07 18:57:58 +0800

    commit in typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,21 @@
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            title: 'Caching'
-        })
-    ],
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ]
+    },
     output: {
-        filename: '[name].[chunkhash].js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     }
 };
